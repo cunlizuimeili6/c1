42aad0aad28906cdd3f4759d027b7f4500ed0fec
Your Name <zhangxueying@qq.com>
2019-01-15 15:38:45 +0800

    first init

---
+++ main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(){
+    int qian=0; 
+    
+    printf("cunqianguanlixitong\n");
+    while(1){
+        int a=0;
+    printf("1---cunqian\n");
+    printf("2---quqian\n");
+    printf("3---yue\n");
+    printf("4---tuichu\n");
+    printf("gongnengbianhao\n");
+    scanf("%d",&a);
+    if(a==1){
+        int cun;
+        char x;
+    printf("cunqianjine\n");
+    scanf("%d",&cun);
+    qian+=cun;
+    printf("jixu\n");
+    scanf("%c",&x);
+    scanf("%c",&x);
+    }
+    }
+
+    return 0;
+
+}
